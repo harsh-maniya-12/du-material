c5483b52ba806adb3eb0b615d6411b0f32353099
maniya harsh <153220392+harsh-maniya-12@users.noreply.github.com>
2025-02-19 10:53:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(fileUpload()); // ✅ Ensure file upload middleware is before routes
 // CORS configuration
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "frontend-ja5h.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
