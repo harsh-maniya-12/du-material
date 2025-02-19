7adf1b8ba1e664cb664caef9bc6b2d49d39d461f
maniya harsh <153220392+harsh-maniya-12@users.noreply.github.com>
2025-02-19 12:33:37 +0530

    Update admin.route.js

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,13 +1,11 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/admin.controller.js";
 
+const router = express.Router();
 
-
-
-const router = express.Router(); // Keep the local declaration as 'router'
-router.post("/signup",signup); // "/signup" is endpoint for client and "signup" is callback function  
-router.post("/login",login);
-
-router.get("/logout",logout);
+// Define routes with the correct path
+router.post("/admin/signup", signup);
+router.post("/admin/login", login);
+router.get("/admin/logout", logout);
 
 export default router;
